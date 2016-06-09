a73699f2d40ad229b139583e7c939c07f2a6d4d8
Petko Bordjukov <bordjukov@gmail.com>
2016-06-09 07:30:47 +0300

    Silence a warning for a local variable

--- lib/crunch/recipes.rb
+++ lib/crunch/recipes.rb
@@ -31,8 +31,8 @@ module Crunch
                           parameters['target']
                         end
         unless parameters['patch_files'].nil?
-          recipe.patch_files = parameters['patch_files'].map do |file|
-            File.expand_path("../../../ports/patches/#{name}/#{file}", __FILE__)
+          recipe.patch_files = parameters['patch_files'].map do |patch|
+            File.expand_path("../../../ports/patches/#{name}/#{patch}", __FILE__)
           end
         end
         recipe.configure_options = parameters['configure_options'] unless parameters['configure_options'].nil?
